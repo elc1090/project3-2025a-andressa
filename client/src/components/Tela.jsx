e78d17c01dde138391c3fba72791be375a9ab4d6
andressateixeira13 <andressateixeira380@gmail.com>
2025-05-27 20:02:25 -0300

    fix: ajuste function

--- client/src/components/Tela.jsx
+++ client/src/components/Tela.jsx
@@ -3,7 +3,7 @@ import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 import "./tela.css";
 
-const Dashboard = () => {
+function Tela() {
   const [mapas, setMapas] = useState([]);
   const navigate = useNavigate();
 
