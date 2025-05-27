968488e586dfc7acad44c0fe528b0e3cea1ea657
andressateixeira13 <andressateixeira380@gmail.com>
2025-05-27 14:59:08 -0300

    feat: teste deploy frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
 import MindMap from './components/MindMap';
 
 function App() {
   return (
-    <div style={{ height: '100vh' }}>
-      <MindMap />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/mindmap" element={<MindMap />} />
+      </Routes>
+    </Router>
   );
 }
 
