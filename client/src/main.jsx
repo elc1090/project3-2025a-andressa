d7839a58459c3265170be71774258fe36e31c71b
andressateixeira13 <andressateixeira380@gmail.com>
2025-05-27 20:48:01 -0300

    teste

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
+import Login from "./components/Login.jsx";
 import Register from "./components/Register.jsx";
 import Tela from "./components/Tela.jsx";
 import MindMap from "./components/MindMap.jsx";
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/tela" element={<Tela />} />
       <Route path="/mapa" element={<MindMap/>}/>
