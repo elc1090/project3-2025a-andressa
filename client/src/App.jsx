e9a135b33367ada18d735669a71d7e804b8b3ff6
andressateixeira13 <andressateixeira380@gmail.com>
2025-05-28 23:02:01 -0300

    teste

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import Login from './components/Login';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Tela from "./components/Tela";
+import MindMap from "./components/MindMap";
+import TelaTeste from "./components/TelaTeste";
 
 function App() {
   return (
-    <div style={{ height: '100vh' }}>
-      <Login />
-    </div>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/tela" element={<Tela />} />
+      <Route path="/mapa" element={<MindMap />} />
+      <Route path="/mapa/:id" element={<MindMap />} />
+      <Route path="/telateste" element={<TelaTeste />} />
+    </Routes>
   );
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,24 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import Login from "./components/Login.jsx";
-import Register from "./components/Register.jsx";
-import Tela from "./components/Tela.jsx";
-import MindMap from "./components/MindMap.jsx";
 import "./index.css";
-import TelaTeste from "./components/TelaTeste.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/tela" element={<Tela />} />
-      <Route path="/mapa" element={<MindMap/>}/>
-      <Route path="/mapa/:id" element={<MindMap />} />
-      <Route path="/telateste" element={<TelaTeste />} />
-    </Routes>
+    <App />
   </BrowserRouter>
 );
